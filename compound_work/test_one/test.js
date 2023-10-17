14eee9c7d37b5f0e2ad80185e159066ed6f2db7c
Naavid Morshed <naavid.m@gmail.com>
2023-10-17 10:49:51 +0600

    afs

---
+++ compound_work/test_one/test.js
@@ -0,0 +1,22 @@
+function showAlert(formInput){
+    // var str = document.getElementById("id1").value; .value takes value from input with id1 but this is not working 
+    // var str = document.getElementById("id1").name;
+    var formInput = formInput;
+    alert(formInput);
+}
+
+function multiplier(num, multiply_by){
+    alert(num * multiply_by);
+    // document.write(num * multiply_by);
+}
+
+// var add=new Function("num1","num2","return num1+num2");  
+
+
+// class employee {
+//     constructor(mail, name, id) {
+//         this.mail = mail;
+//         this.name = name;
+//         this.id = id;
+//     }
+// }
---
+++ compound_work/test_two/modal_with_form.js
@@ -0,0 +1,7 @@
+function showAlert() {
+    alert("You typed: " + document.form_element_1.fe1_1.value);
+}
+
+function alertByID(){
+    alert("This is what you have typed: " + document.getElementById('fe1_2').value);
+}
