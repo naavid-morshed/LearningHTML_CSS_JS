6692d78837f68fc29a85998f4caa4d2897291bb7
Naavid Morshed <naavid.m@gmail.com>
2024-02-13 18:54:37 +0600

    started learning core CSS, going good

--- compound_work/clock/clock.js
+++ compound_work/clock/clock.js
@@ -1,16 +1,34 @@
 function checkDate(id) {
-  const date = new Date();
-  document.getElementById(id).innerHTML = date.toLocaleDateString();
+    const date = new Date();
+    document.getElementById(id).innerHTML = date.toLocaleDateString();
 }
 
 function checkTimer(id) {
-  const date = new Date();
-  const hours = date.getHours() < 10 ? "0".concat(date.getHours()) : date.getHours();
-  const minutes = date.getMinutes() < 10 ? "0".concat(date.getMinutes()) : date.getMinutes();
-  const seconds = date.getSeconds() < 10 ? "0".concat(date.getSeconds()) : date.getSeconds();
-  const mil = date.getMilliseconds() < 10 ? "00".concat(date.getMilliseconds()) 
-            : date.getMilliseconds() < 100? "0".concat(date.getMilliseconds()) 
-            : date.getMilliseconds();
-  const am_pm = hours > 11 ? " PM" : " AM";
-  document.getElementById(id).innerHTML = hours + ":" + minutes + ":" + seconds + ":" + mil + am_pm;
+    const date = new Date();
+
+    const hours =
+        date.getHours() < 10 ?
+            "0".concat(date.getHours().toString())
+            : date.getHours().toString();
+
+    const minutes =
+        date.getMinutes() < 10 ?
+            "0".concat(date.getMinutes().toString())
+            : date.getMinutes().toString();
+
+    const seconds =
+        date.getSeconds() < 10 ?
+            "0".concat(date.getSeconds().toString())
+            : date.getSeconds().toString();
+
+    const mil =
+        date.getMilliseconds() < 10 ?
+            "00".concat(date.getMilliseconds().toString())
+            : date.getMilliseconds() < 100 ?
+                "0".concat(date.getMilliseconds().toString())
+                : date.getMilliseconds().toString();
+
+    const am_pm = hours > 11 ? " PM" : " AM";
+
+    document.getElementById(id).innerHTML = hours + ":" + minutes + ":" + seconds + ":" + mil + am_pm;
 }
